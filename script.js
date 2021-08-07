e6533cf6cd711cc37c3a6911453565a88236b048
Varshitha941 <varshitha.gaddipati@gmail.com>
2021-08-07 16:08:12 +0530

    Updated the website

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        if(this.scroll > 500){
+        if (this.scroll > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
-           $('.scroll-up-btn').removeClass("show"); 
+        } else {
+            $('.scroll-up-btn').removeClass("show");
         }
     });
 
     //slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop:0});
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
     })
 
     //toggle menu / navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass("active")
         $('.menu-btn i').toggleClass("active")
     });
 
     //typing animation
-    var typed = new Typed(".typing",{
+    var typed = new Typed(".typing", {
         strings: ["Student", "Tech enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
@@ -38,20 +38,20 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-/*           
-            1000:{
+
+            1000: {
                 items: 3,
                 nav: false
             }
-*/
+
         }
     })
 })
